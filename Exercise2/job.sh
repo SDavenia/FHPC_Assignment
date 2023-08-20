cc5d56c8f96571600e6e2cbae2d3c2da0220c4f9
SDavenia <davesam00@gmail.com>
2023-08-20 20:15:35 +0200

    First run

--- Exercise2/job.sh
+++ Exercise2/job.sh
@@ -6,13 +6,20 @@
 #SBATCH --cpus-per-task=64
 #SBATCH --mem=200gb 
 #SBATCH --time=00:05:00 
-#SBATCH --output=attempt.csv
+
+module load architecture/AMD
+module load mkl
+module load openBLAS/0.3.23-omp
+
+srun -n1 make cpu
+
 for size in 2000 4000 6000
 do
     output_file_name="size_$size"
-    echo $output_file_name 
-    echo "Running using matrix of size $size"
+    echo "Running using matrix of size $size" > "$output_file_name.txt"
+    echo "MKL library" >> "$output_file_name.txt"
     srun -n1 --cpus-per-task=64 ./gemm_mkl.x $size $size $size >> "$output_file_name.txt"
+    echo "BLAS library" >> "$output_file_name.txt"
     srun -n1 --cpus-per-task=64 ./gemm_oblas.x $size $size $size >> "$output_file_name.txt"
     echo ---------------------------------------------------------------------------------------------------------  
 done
