4fa0ffe878c22c2048319928a06c26f63f2018ae
SDavenia <davesam00@gmail.com>
2023-08-21 11:56:24 +0200

    Attempt

---
+++ Exercise2/attempt/MKL.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    int size;
+    if (argc > 1)
+        size = atoi(argv[1]);
+    else{
+        printf("You need to pass the size\n");
+        return 0;
+    }
+    
+    printf("Size: %dx%dx%d\tTime: %d s\tGFLOPS: %d\n", size, size, size, size/90, size*60);
+
+    
+
+}
+
+
+/*
+Using double
+
+ Computing matrix product using gemm function via CBLAS interface
+
+ Elapsed time 0.62182575 s
+
+
+2000x2000x2000  0.062183 s      257.306810 GFLOPS
+BLAS library
+
+ This example computes real matrix C=alpha*A*B+beta*C using
+ BLAS function dgemm, where A, B, and  C are matrices and
+ alpha and beta are scalars
+
+ Initializing data for matrix multiplication C=A*B for matrix
+ A(2000x2000) and matrix B(2000x2000)
+
+ Using double
+
+ Computing matrix product using gemm function via CBLAS interface
+
+ Elapsed time 0.68869930 s
+
+
+2000x2000x2000  0.068870 s      232.322002 GFLOPS
+*/
--- Exercise2/attempt/trial.sh
+++ Exercise2/attempt/trial.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
-gcc trial.c -o trial.x
-rm output.csv # Remove previous file.
+gcc OB.c -o OB.x
+gcc MKL.c -o MKL.x
+
+rm -rf *.csv # Remove previous csv file.
+
 for i in 1000 2000 3000
 do
-    echo $i
-    ./trial.x $i > output.txt
 
-    # Extract information using grep and regular expressions
-    size=$(grep -o 'Size: [0-9]*' output.txt | cut -d' ' -f2)
-    time=$(grep -o 'Time: [0-9]*' output.txt | cut -d' ' -f2)
-    gflops=$(grep -o 'GFLOPS: [0-9]*' output.txt | cut -d' ' -f2)
+    # Run everything with openBLAS
+    for j in 1 2 3 4 5 # Take multiple measurements
+    do
+        ./OB.x $i > output.txt #just a temporary file
 
-    # Store the extracted information in a CSV file
-    #!/bin/bash
-    if [ ! -e "output.csv" ]; then
-        echo "Size,Time,GFLOPS" > output.csv
-    fi
-    echo "$size,$time,$gflops" >> output.csv
+        # Extract information using grep and regular expressions
+        size=$(grep -o 'Size: [0-9]*' output.txt | cut -d' ' -f2)
+        time=$(grep -o 'Time: [0-9]*' output.txt | cut -d' ' -f2)
+        gflops=$(grep -o 'GFLOPS: [0-9]*' output.txt | cut -d' ' -f2)
+        # Store the extracted information in a CSV file
+        filename="OBLAS_$size.csv"
 
-    echo "Extracted information has been stored in output.csv"
+        if [ ! -e $filename ]; then
+            echo "Size,Time,GFLOPS" > $filename
+        fi
+        echo "$size,$time,$gflops" >> $filename
+    done
+
+    # Now repeat for MKL library
+    for j in 1 2 3 4 5
+    do
+        ./MKL.x $i > output.txt #just a temporary file
+
+        # Extract information using grep and regular expressions
+        size=$(grep -o 'Size: [0-9]*' output.txt | cut -d' ' -f2)
+        time=$(grep -o 'Time: [0-9]*' output.txt | cut -d' ' -f2)
+        gflops=$(grep -o 'GFLOPS: [0-9]*' output.txt | cut -d' ' -f2)
+        # Store the extracted information in a CSV file
+        filename="MKL_$size.csv"
+
+        if [ ! -e $filename ]; then
+            echo "Size,Time,GFLOPS" > $filename
+        fi
+        echo "$size,$time,$gflops" >> $filename
+    done
+
 done
